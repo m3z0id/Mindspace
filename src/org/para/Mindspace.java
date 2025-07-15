5993e24a86157a560d6fb8a0f370c6042cdee4fa
vima <bigm3z0id@gmail.com>
2025-07-15 14:39:10 +0200

    Changed it into our own

--- src/org/para/Mindspace.java
+++ src/org/para/Mindspace.java
@@ -1,17 +1,14 @@
-package example;
+package org.para;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class Mindspace extends Mod {
 
-    public ExampleJavaMod(){
+    public Mindspace(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
